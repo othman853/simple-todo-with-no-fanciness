4c34deab15bee55e18408421ac9d32d7aef733b7
othman853 <anuar.yasser@gmail.com>
2016-04-16 18:43:46 -0300

    Listening to some socket events.

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 
 var express = require('express');
 var app = express();
+var io = require('socket.io');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/' , (request, response) => response.render('index.ejs', {title: 'Hello'} ));
+app.get('/' , (request, response) => response.render('new-todo.ejs'));
 
-app.listen(3000, () => console.log('Up on port 3000'));
+var httpServer = require('http').createServer(app);
+var socketClient = io(httpServer);
+
+httpServer.listen(3000, () => console.log('Up on port 3000'));
+
+socketClient.on('connect', (client) => {
+
+  client.on('new todo step 1', (data) => {
+    client.emit('new todo step 1 response', {message: data.title + ' was created'});
+  });
+
+});
