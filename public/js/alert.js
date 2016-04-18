4f9902582c71a0185721c96dd56e1c50f3612a14
othman853 <anuar.yasser@gmail.com>
2016-04-18 15:34:52 -0300

    Modularizing alert operations

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,7 +1,36 @@
-$('.alert > .close > span').map(function (alert) {
+var Todos = Todos || {};
 
-  $(alert).on('click', function() {
-    console.log('Close.');
-  });
+Todos.Alert = function Alert() {
 
-});
+  console.log('New Todo.Alert.');
+
+  var template =
+  "<div class='alert-boilerplate alert alert-dismissible' role='alert'> " +
+    "<button type='button' class='close' data-dismiss='alert' aria-label='Close'> " +
+      "<span class='close-span' aria-hidden='true'>&times;</span> " +
+    "</button> " +
+    "<span class='alert-text'></span> " +
+  "</div>";
+
+  var create = function create(message, messageType) {
+    var alert = $(template);
+
+    alert.removeClass('alert-boilerplate');
+    alert.addClass('alert-' + messageType);
+
+    $(alert.find('.alert-text')).html(message);
+
+    var closeButton = $(alert.find('.close-span'));
+
+    closeButton.on('click', function() {
+      alert.remove();
+    });
+
+    return alert;
+  };
+
+  return {
+    create : create
+  };
+
+};
